e1031dbebace3d872d0190e37f250e00b2ea7027
vinhxuan20004 <tranvinhxuan1510@gmail.com>
2026-04-04 23:50:55 +0700

    rebase

--- java-backend/src/main/java/com/fashionstore/repository/VoucherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fashionstore.repository;
-
-import com.fashionstore.model.Voucher;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import java.util.Optional;
-
-public interface VoucherRepository extends MongoRepository<Voucher, String> {
-    Optional<Voucher> findByCode(String code);
-}
